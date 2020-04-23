fd780dcf1202658c6112dad34a444490d2ca86d6
slinden2 <samu.linden@gmail.com>
2020-04-23 12:02:24 +0200

    Add cors support

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "source-map-support/register";
 
 import config from "./config";
@@ -8,6 +9,7 @@ import { calendarRoute } from "./routes/";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/calendar", calendarRoute);
 
