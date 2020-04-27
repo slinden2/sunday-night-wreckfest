f4a4eac55ef4d17b510a6131a3997977aa77d879
slinden2 <samu.linden@gmail.com>
2020-04-27 16:37:44 +0200

    Test cleanup

--- backend/src/services/getRaceCalendarUtils.test.ts
+++ backend/src/services/getRaceCalendarUtils.test.ts
@@ -1,8 +1,4 @@
 import {
-  isString,
-  parseEventId,
-  isNumber,
-  parseNumericBoolean,
   isDate,
   parseDate,
   parseString,
@@ -11,64 +7,16 @@ import {
 } from "./getRaceCalendarUtils";
 import { DataIntegrityError } from "../utils/errors";
 
-describe("googleSheetsUtils", () => {
-  describe("isString", () => {
-    it("should return true with an empty string", () => {
-      expect(isString("")).toBeTruthy;
-    });
-    it("should return true with a string", () => {
-      expect(isString("test string")).toBeTruthy;
-    });
-    it("should return false with a number", () => {
-      expect(isString(1)).toBeFalsy;
-    });
-    it("should return false with a boolean", () => {
-      expect(isString(true)).toBeFalsy;
-    });
-  });
-  describe("parseEventId", () => {
-    it("should throw an error with longer than 4 char string", () => {
-      expect(() => parseEventId("12345")).toThrow(DataIntegrityError);
-    });
-    it("should throw an error with shorter than 4 char string", () => {
-      expect(() => parseEventId("123")).toThrow(DataIntegrityError);
-    });
-  });
-  describe("isNumber", () => {
-    it("should be falsy with a string", () => {
-      expect(isNumber("x")).toBeFalsy();
-    });
-    it("should be truthy with a string number", () => {
-      expect(isNumber("1")).toBeTruthy();
-    });
-    it("should be truthy with a number", () => {
-      expect(isNumber(1)).toBeTruthy();
-    });
-  });
-  describe("parseNumericBoolean", () => {
-    it("should be truthy with 1", () => {
-      expect(parseNumericBoolean("1", "test")).toBeTruthy;
-    });
-    it("should be falsy with 0", () => {
-      expect(parseNumericBoolean("0", "test")).toBeFalsy;
-    });
-    it("should throw an error if the number is not 0 or 1", () => {
-      expect(() => parseNumericBoolean("4", "test")).toThrow(
-        DataIntegrityError
-      );
-    });
-    it("should throw an error if the number is not 0 or 1", () => {
-      expect(() => parseNumericBoolean("-4", "test")).toThrow(
-        DataIntegrityError
-      );
-    });
-  });
+describe("getRaceCalendarUtils", () => {
   describe("isDate", () => {
     it("should be truthy with 2020-01-01", () => {
-      expect(isDate("2020-01-01")).toBeTruthy;
+      expect(isDate("2020-01-01")).toBeTruthy();
     });
     it("should be truthy with 01-01-2020", () => {
-      expect(isDate("01-01-2020")).toBeFalsy;
+      expect(isDate("01-01-2020")).toBeTruthy();
+    });
+    it("should be falsy with 'testdate'", () => {
+      expect(isDate("testdate")).toBeFalsy();
     });
   });
   describe("parseDate", () => {
--- backend/src/services/getRaceCalendarUtils.ts
+++ backend/src/services/getRaceCalendarUtils.ts
@@ -1,4 +1,10 @@
 import { DataIntegrityError } from "../utils/errors";
+import {
+  isString,
+  parseEventId,
+  isNumber,
+  parseNumericBoolean,
+} from "./googleSheetsServiceUtils";
 
 export interface RaceCalendarEvent {
   eventId: string;
@@ -9,34 +15,6 @@ export interface RaceCalendarEvent {
   qLaps: number;
   raceLaps: number;
 }
-
-export const isString = (param: any): param is string => {
-  return typeof param === "string" || param instanceof String;
-};
-
-export const parseEventId = (eventId: string): string => {
-  if (!eventId || !isString(eventId) || eventId.length !== 4) {
-    throw new DataIntegrityError("Invalid or missing eventId: " + eventId);
-  }
-
-  return eventId;
-};
-
-export const isNumber = (param: any): param is number => {
-  return !isNaN(Number(param)) && typeof param !== "boolean";
-};
-
-export const parseNumericBoolean = (param: string, field: string): boolean => {
-  if (
-    !param ||
-    !isNumber(param) ||
-    (Number(param) !== 0 && Number(param) !== 1)
-  ) {
-    throw new DataIntegrityError(`Invalid or missing ${field}: ${param}`);
-  }
-
-  return Boolean(Number(param));
-};
 
 export const isDate = (date: string): boolean => {
   return Boolean(Date.parse(date));
