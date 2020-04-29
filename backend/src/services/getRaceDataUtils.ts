049ed683a4ffb304503c7212299b179de511ac6a
slinden2 <samu.linden@gmail.com>
2020-04-29 17:15:16 +0200

    Console.log removed

--- backend/src/services/getRaceDataUtils.ts
+++ backend/src/services/getRaceDataUtils.ts
@@ -2,7 +2,6 @@ import { IDriverSeasonRaceData } from "../types";
 import Race from "./race";
 
 export const toRaceData = (driverData: IDriverSeasonRaceData[]) => {
-  console.log(driverData);
   const raceData = new Race(driverData);
   return raceData.getRaceData;
 };
