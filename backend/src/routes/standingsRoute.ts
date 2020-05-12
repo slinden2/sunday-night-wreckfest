414290b7abc5935e93e9862435b1d2b01d9816ee
Samu <samu.linden@gmail.com>
2020-05-12 15:24:58 +0200

    Temp standingsRoute fix

--- backend/src/routes/standingsRoute.ts
+++ backend/src/routes/standingsRoute.ts
@@ -12,13 +12,13 @@ router.get("/", async (_req, res, next) => {
   }
 });
 
-// router.get("/:id", async (req, res, next) => {
-//   try {
-//     const standings = await standingsService.getStandings(req.params.id);
-//     res.status(200).json(standings);
-//   } catch (err) {
-//     next(err);
-//   }
-// });
+router.get("/:id", async (req, res, next) => {
+  try {
+    const standings = await standingsService.getStandings(req.params.id);
+    res.status(200).json(standings);
+  } catch (err) {
+    next(err);
+  }
+});
 
 export default router;
