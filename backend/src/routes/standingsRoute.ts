67107dd9b96bc911145b00e7904ff4993301f1f6
Samu <samu.linden@gmail.com>
2020-05-12 13:56:13 +0200

    Temporary standings route added

--- backend/src/routes/standingsRoute.ts
+++ backend/src/routes/standingsRoute.ts
@@ -3,13 +3,22 @@ import { standingsService } from "../services";
 
 const router = express.Router();
 
-router.get("/:id", async (req, res, next) => {
+router.get("/", async (_req, res, next) => {
   try {
-    const standings = await standingsService.getStandings(req.params.id);
+    const standings = await standingsService.getStandings("0400");
     res.status(200).json(standings);
   } catch (err) {
     next(err);
   }
 });
+
+// router.get("/:id", async (req, res, next) => {
+//   try {
+//     const standings = await standingsService.getStandings(req.params.id);
+//     res.status(200).json(standings);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 export default router;
