e0166d3a099943cf123b5990eb7265dd503f13c1
slinden2 <slinden2@users.noreply.github.com>
2020-05-19 13:07:19 +0000

    Automated production bundle creation

--- dist/app.js
+++ dist/app.js
@@ -19,14 +19,14 @@ if (config_1.default.ENV === "test" || config_1.default.ENV === "CI") {
         res.send("pong");
     });
 }
+app.use("/api/races", routes_1.raceRoute);
+app.use("/api/standings", routes_1.standingsRoute);
 if (config_1.default.ENV === "production") {
     app.use(express_1.default.static(path_1.default.join(__dirname, "client")));
-    app.get("/", (_req, res) => {
+    app.get("*", (_req, res) => {
         res.sendFile(path_1.default.resolve(__dirname, "client", "index.html"));
     });
 }
-app.use("/api/races", routes_1.raceRoute);
-app.use("/api/standings", routes_1.standingsRoute);
 app.use(middleware_1.default.errorHandler);
 app.use(middleware_1.default.unknownEndpoint);
 exports.default = app;
