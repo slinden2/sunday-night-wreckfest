2439023d92376a01b56e6d4bfd7518eec763324c
Samu <samu.linden@gmail.com>
2020-05-29 15:28:02 +0200

    Loading indicator styles

--- frontend/src/components/LoadingIndicator.tsx
+++ frontend/src/components/LoadingIndicator.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import styled from "styled-components";
+
+import whiteGrit from "../assets/whitegrit.png";
+import blackGrit from "../assets/blackgrit.png";
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const GritContainer = styled.div`
+  margin-top: 5rem;
+  font-size: 3rem;
+  font-weight: 800;
+  position: relative;
+  margin-right: -2rem;
+  background: url(${blackGrit}) 0% 0% / 600px;
+  padding: 2rem;
+  z-index: 1;
+  display: inline-block;
+
+  &::after {
+    position: absolute;
+    z-index: 2;
+    content: "";
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+    pointer-events: none;
+    background: url(${whiteGrit}) center center / 700px repeat;
+    background-repeat: repeat;
+    background-position: center center;
+    z-index: 2;
+  }
+`;
 
 const LoadingIndicator = () => {
-  return <div>Loading...</div>;
+  return (
+    <Container>
+      <GritContainer>
+        <span>Loading...</span>
+      </GritContainer>
+    </Container>
+  );
 };
 
 export default LoadingIndicator;
