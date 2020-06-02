ccecd82a9dd9cc8464e29744b550d24d7d0bd9dc
slinden2 <slinden2@users.noreply.github.com>
2020-06-02 13:00:49 +0000

    Automated production bundle creation

---
+++ dist/client/precache-manifest.36ced1061030b13fc38f712574616dd9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d2e80a5e8bd619ab669896a33b25de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20717b5b79092ef9df80",
+    "url": "/static/js/2.29b6c64b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.29b6c64b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "033ede06fe4c3ed1dd27",
+    "url": "/static/js/main.1b7dbaad.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "15c168539fb7109ce300574e7b4b0732",
+    "url": "/static/media/blackgrit.15c16853.png"
+  },
+  {
+    "revision": "5934c57c7ed687638cdd88a42a28b311",
+    "url": "/static/media/logo.5934c57c.svg"
+  },
+  {
+    "revision": "3e67bed838cd933911fa64a76a43624f",
+    "url": "/static/media/whitegrit.3e67bed8.png"
+  }
+]);
--- dist/services/event/eventUtils.js
+++ dist/services/event/eventUtils.js
@@ -62,7 +62,7 @@ exports.toSeasonDetails = (id, seasonData) => {
     if (!rawSeason) {
         return undefined;
     }
-    return Object.assign(Object.assign({ seasonId: helpers_1.parseEventId(rawSeason.seasonId, "seasonId"), seasonName: helpers_1.parseString(rawSeason.seasonName, "seasonName"), description: helpers_1.parseString(rawSeason.description, "description") }, (rawSeason.cars
+    return Object.assign(Object.assign({ seasonId: helpers_1.parseEventId(rawSeason.seasonId, "seasonId"), seasonName: helpers_1.parseString(rawSeason.seasonName, "seasonName"), description: helpers_1.parseDescription(rawSeason.description) }, (rawSeason.cars
         ? {
             cars: helpers_1.parseCars(rawSeason.cars),
         }
