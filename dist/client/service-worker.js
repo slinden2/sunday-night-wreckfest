8c85b091fcda743bc4357d9a2d90d84dcccb73ff
slinden2 <slinden2@users.noreply.github.com>
2020-06-05 08:30:24 +0000

    Automated production bundle creation

--- dist/client/service-worker.js
+++ dist/client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e757e208d41bd2c621bc40d331dfa22.js"
+  "/precache-manifest.2f93d1d286de94ded479157b611ac17c.js"
 );
 
 self.addEventListener('message', (event) => {
