8df145747152f3982429f935b28186769146f1eb
Samu <samu.linden@gmail.com>
2020-06-05 16:02:54 +0200

    Return undefined if no races found

--- backend/src/services/event/eventService.ts
+++ backend/src/services/event/eventService.ts
@@ -12,9 +12,14 @@ import config from "../../config";
 
 export const getRaceData = async (
   id: string
-): Promise<IDriverSeasonRaceData[]> => {
+): Promise<IDriverSeasonRaceData[] | undefined> => {
   const eventDetails = await getSheetAndRows("eventDetails");
   const driverRaceDetails = toDriverRaceDetails(id, eventDetails.rows);
+
+  if (!driverRaceDetails.length) {
+    return undefined;
+  }
+
   const raceData = new Race(driverRaceDetails);
   return raceData.getRaceData;
 };
@@ -72,20 +77,23 @@ export const checkDraws = async () => {
   for (const event of eventList) {
     if (event.isReady && event.isCompleted && !event.isProcessed) {
       const raceData = await getRaceData(event.eventId);
-      const rowsToVerify = getDraws(raceData);
-      if (rowsToVerify.length) {
-        const eventDetails = await getSheetAndRows("eventDetails");
-        const promises: Promise<any>[] = [];
-        rowsToVerify.forEach(driver => {
-          const rowToUpdate = eventDetails.rows.find(
-            row =>
-              row.eventId === driver.eventId && row.driverId === driver.driverId
-          );
-          rowToUpdate.drawPosition = config.CHECK_DRAW_TEXT;
-          promises.push(rowToUpdate.save({ raw: true }));
-        });
+      if (raceData) {
+        const rowsToVerify = getDraws(raceData);
+        if (rowsToVerify.length) {
+          const eventDetails = await getSheetAndRows("eventDetails");
+          const promises: Promise<any>[] = [];
+          rowsToVerify.forEach(driver => {
+            const rowToUpdate = eventDetails.rows.find(
+              row =>
+                row.eventId === driver.eventId &&
+                row.driverId === driver.driverId
+            );
+            rowToUpdate.drawPosition = config.CHECK_DRAW_TEXT;
+            promises.push(rowToUpdate.save({ raw: true }));
+          });
 
-        await Promise.all(promises);
+          await Promise.all(promises);
+        }
       }
     }
   }
