4ac7993e49773c48f6c78cbeb565b75cf3eb9397
slinden2 <samu.linden@gmail.com>
2020-06-07 18:23:12 +0200

    Tests fixed

--- backend/src/routes/steamRoute.test.ts
+++ backend/src/routes/steamRoute.test.ts
@@ -2,12 +2,35 @@ import supertest from "supertest";
 
 import app from "../app";
 import { steamService } from "../services";
+import { toPromise } from "../utils/mockData";
 
 const api = supertest(app);
 
 describe("GET /api/servers", () => {
   beforeEach(() => {
-    jest.spyOn(steamService, "getSNWServers").mockImplementation();
+    jest.spyOn(steamService, "getSNWServers").mockImplementation(() =>
+      toPromise([
+        {
+          addr: "string",
+          gameport: 1,
+          steamid: "string",
+          name: "string",
+          appid: 1,
+          gamedir: "string",
+          version: "string",
+          product: "string",
+          region: 1,
+          players: 1,
+          maxPlayers: 1,
+          bots: 1,
+          map: "string",
+          secure: true,
+          dedicated: true,
+          os: "string",
+          gametype: "string",
+        },
+      ])
+    );
   });
 
   afterEach(() => {
@@ -15,7 +38,9 @@ describe("GET /api/servers", () => {
   });
 
   it("should return valid json", async () => {
-    await api.get("/api/standings").expect(200);
-    // .expect("Content-Type", /application\/json/);
+    await api
+      .get("/api/servers")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
   });
 });
---
+++ backend/src/services/steam/steamService.test.ts
@@ -0,0 +1,56 @@
+import axios from "axios";
+
+import * as steamUtils from "./steamUtils";
+import { getSNWServers } from "./steamService";
+import { toPromise } from "../../utils/mockData";
+
+jest.mock("axios");
+
+describe("steamService", () => {
+  beforeEach(() => {
+    jest
+      .spyOn(steamUtils, "dnsLookup")
+      .mockImplementation(() => toPromise("192.168.1.10"));
+    // eslint-disable-next-line
+    // @ts-ignore
+    axios.get.mockImplementationOnce(() =>
+      Promise.resolve({
+        data: {
+          response: {
+            servers: [
+              {
+                addr: "192.168.1.10",
+                gameport: 33544,
+                steamid: "string",
+                name: "string",
+                appid: 1,
+                gamedir: "string",
+                version: "string",
+                product: "string",
+                region: 1,
+                players: 1,
+                // eslint-disable-next-line @typescript-eslint/camelcase
+                max_players: 1,
+                bots: 1,
+                map: "string",
+                secure: true,
+                dedicated: true,
+                os: "string",
+                gametype: "string",
+              },
+            ],
+          },
+        },
+      })
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return an array of servers", async () => {
+    const servers = await getSNWServers();
+    expect(servers).toEqual([{ name: "string", players: 1, maxPlayers: 1 }]);
+  });
+});
