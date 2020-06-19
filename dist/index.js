56a01bf8ecfc8b8798532715a03084aa1909b7c8
slinden2 <slinden2@users.noreply.github.com>
2020-06-19 16:58:49 +0000

    Automated production bundle creation

--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,7 @@ const jobs_1 = require("./jobs");
 const server = http_1.default.createServer(app_1.default);
 server.listen(config_1.default.PORT, () => {
     logger_1.default.info(`Server running on port ${config_1.default.PORT}`);
-    node_cron_1.default.schedule("5 * * * *", () => {
+    node_cron_1.default.schedule("(*/5 * * * *", () => {
         logger_1.default.info("Cron started");
         jobs_1.updateCache().catch(err => logger_1.default.error(err));
         logger_1.default.info("Cron finished");
