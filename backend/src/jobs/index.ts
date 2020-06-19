2b5132d474f3e25774a6a9f095d89daa8470b869
Samu <samu.linden@gmail.com>
2020-06-19 18:45:31 +0200

    Added some clg for debugging

--- backend/src/jobs/index.ts
+++ backend/src/jobs/index.ts
@@ -1,16 +1,20 @@
 import Redis from "ioredis";
 import { calendarService, standingsService, teamService } from "../services";
 import config from "../config";
+import logger from "../utils/logger";
 
 const redis = new Redis(config.REDIS_URL);
 
 export const updateCache = async () => {
   const raceCalendar = await calendarService.getRaceCalendar();
   await redis.setex("/api/races", 320, JSON.stringify(raceCalendar));
+  logger.info("Calendar updated");
 
   const standings = await standingsService.getStandings();
   await redis.setex("/api/standings", 320, JSON.stringify(standings));
+  logger.info("Standings updated");
 
   const teams = await teamService.getTeams();
   await redis.setex("/api/teams", 320, JSON.stringify(teams));
+  logger.info("Teams updated");
 };
