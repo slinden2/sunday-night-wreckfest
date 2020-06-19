ad42f0f543263b991c3cf99267d376882438c316
Samu <samu.linden@gmail.com>
2020-06-19 19:11:18 +0200

    Removed debugging clgs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,8 +12,6 @@ server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 
   cron.schedule("(*/5 * * * *", () => {
-    logger.info("Cron started");
     updateCache().catch(err => logger.error(err));
-    logger.info("Cron finished");
   });
 });
