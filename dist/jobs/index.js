da0c7bce95b07694af3db147da50ad7f324c9290
slinden2 <slinden2@users.noreply.github.com>
2020-06-23 11:40:31 +0000

    Automated production bundle creation

--- dist/jobs/index.js
+++ dist/jobs/index.js
@@ -16,22 +16,27 @@ const ioredis_1 = __importDefault(require("ioredis"));
 const services_1 = require("../services");
 const config_1 = __importDefault(require("../config"));
 const misc_1 = require("../utils/misc");
+const logger_1 = __importDefault(require("../utils/logger"));
 const redis = new ioredis_1.default(config_1.default.REDIS_URL);
 const updateRaceCache = (calendar) => __awaiter(void 0, void 0, void 0, function* () {
-    for (const event of calendar) {
-        console.log(event.eventId);
+    for (const event of calendar.reverse()) {
         const raceData = yield services_1.eventService.getSingleRace(event.eventId);
         yield redis.setex(`/api/races/${event.eventId}`, 86400, JSON.stringify(raceData));
         yield misc_1.sleep(10000);
+        logger_1.default.info(`updateRaceCache - ${event.eventId} done.`);
     }
 });
 exports.updateCache = () => __awaiter(void 0, void 0, void 0, function* () {
+    logger_1.default.info("updateCache - Cache update started.");
     const raceCalendar = yield services_1.calendarService.getRaceCalendar();
     yield redis.setex("/api/races", 86400, JSON.stringify(raceCalendar));
+    logger_1.default.info("updateCache - raceCalendar done.");
     const standings = yield services_1.standingsService.getStandings();
     yield redis.setex("/api/standings", 86400, JSON.stringify(standings));
+    logger_1.default.info("updateCache - standings done.");
     const teams = yield services_1.teamService.getTeams();
     yield redis.setex("/api/teams", 86400, JSON.stringify(teams));
+    logger_1.default.info("updateCache - teams done.");
     yield updateRaceCache(raceCalendar);
 });
 //# sourceMappingURL=index.js.map
